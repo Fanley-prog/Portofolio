628196cc59270133d66832ef63a3f75c99593d4b
Fanley-prog <jevildelta900@gmail.com>
2025-02-27 12:04:16 +0700

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the modal image and caption
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-// Get all images inside the gallery
 var images = document.querySelectorAll(".row img");
 
-// Loop through all images and add click event
 images.forEach(function(img) {
     img.addEventListener("click", function() {
         modal.style.display = "block";
@@ -17,12 +13,10 @@ images.forEach(function(img) {
     });
 });
 
-// Close the modal when clicking the close button
 document.querySelector(".close").addEventListener("click", function() {
     modal.style.display = "none";
 });
 
-// Close the modal when clicking outside the image
 modal.addEventListener("click", function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
