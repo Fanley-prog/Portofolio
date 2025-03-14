3062a142e7a0db15544ca2ffe7b5127e4bd08ff1
algiuu <133425601+algiuu@users.noreply.github.com>
2025-03-14 21:03:37 +0700

    pp

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ modal.addEventListener("click", function(event) {
         modal.style.display = "none";
     }
 });
+
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    document.getElementById('Home').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
